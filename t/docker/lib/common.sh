1661abb101987dbbad7d90ca6fefabccb35641d2
Andrii Nikitin <anikitin@suse.de>
2020-02-21 13:55:45 +0100

    Rework docker tests

---
+++ t/docker/lib/common.sh
@@ -0,0 +1,76 @@
+#!lib/test-in-container-systemd.sh
+
+METHOD=${METHOD:-rsync}
+
+prepare_project() {
+
+prj=$1
+suff=$2
+
+su $dbuser -c "set -ex
+cd /opt/openqa-trigger-from-obs
+mkdir -p $prj
+python3 script/scriptgen.py $prj
+for l in $prj/*/.run_last ; do
+    [ ! -e \$l ] || rm \$l
+done
+[ ! -e $prj/.run_last ] || rm $prj/.run_last
+echo geekotest > rsync.secret
+chmod 600 rsync.secret"
+
+echo "127.0.0.1 obspublish$suff" >> /etc/hosts
+
+mkdir -p /var/lib/openqa/osc-plugin-factory/factory-package-news/
+touch /var/lib/openqa/osc-plugin-factory/factory-package-news/factory-package-news.py
+chmod +x /var/lib/openqa/osc-plugin-factory/factory-package-news/factory-package-news.py
+/var/lib/openqa/osc-plugin-factory/factory-package-news/factory-package-news.py
+
+
+systemctl restart openqa-websockets.service
+
+[ "$METHOD" != rest ] || systemctl restart openqa-gru.service
+
+# wait for webui to become available
+sleep 2
+attempts_left=10
+while ! curl -sI http://localhost/ | grep 200 ; do
+    sleep 3
+    : $((attempts_left--))
+    [ "$attempts_left" -gt 0 ] || {
+        service openqa-webui status
+        exit 1
+    }
+done
+
+# this must create default user
+curl -sI http://localhost/login
+
+# create api key - the table will be available after webui service startup
+API_KEY=$(hexdump -n 8 -e '2/4 "%08X" 1 "\n"' /dev/urandom)
+API_SECRET=$(hexdump -n 8 -e '2/4 "%08X" 1 "\n"' /dev/urandom)
+echo "INSERT INTO api_keys (key, secret, user_id, t_created, t_updated) VALUES ('${API_KEY}', '${API_SECRET}', 2, NOW(), NOW());" | su postgres -c "psql $dbname"
+
+cat >> /etc/openqa/client.conf <<EOF
+[localhost]
+key = ${API_KEY}
+secret = ${API_SECRET}
+EOF
+
+mkdir -p /root/.config/openqa
+cp /etc/openqa/client.conf /root/.config/openqa/
+mkdir -p /var/lib/openqa/.config/openqa/
+cp /etc/openqa/client.conf /var/lib/openqa/.config/openqa/
+chown "$dbuser" /var/lib/openqa/.config/openqa/client.conf
+
+systemctl enable --now rsyncd
+
+if [ "$METHOD" == rest ]; then
+    openqa-client --host localhost /api/v1/obs_rsync/$prj/runs put
+else
+    echo 111 > /opt/openqa-trigger-from-obs/$prj/.job_id
+    su "$dbuser" -c "/opt/openqa-trigger-from-obs/script/rsync.sh $prj"
+fi
+
+sleep 10
+
+}
--- t/docker/lib/test-in-container-systemd.sh
+++ t/docker/lib/test-in-container-systemd.sh
@@ -33,62 +33,12 @@ containername="localtest.${basename,,}"
 
 SKIP test "${PRIVILEGED_TESTS}" != 1 # PRIVILEGED_TESTS is not set to 1
 docker_info="$(docker info >/dev/null 2>&1)" || SKIP test 1 # Docker doesn't seem to be running
-testsetup=${testcase%.*}.setup.sh
-dockerfile=${testcase%.*}.Dockerfile
-echo TESTSETUP=$testsetup
-if [ -f "$testsetup" ] ; then
-    PLAN 2
-else
-    PLAN 1
-fi
-
-# let's use `docker build` here to utilize docker cache
-(
-# shellcheck disable=SC2046 disable=SC2005
-echo "FROM opensuse/leap:15.1
-ENV container docker
-RUN useradd -r -d /usr/lib/openqa -g users --uid=$(id -u) geekotest
-
-RUN zypper -n install systemd
-
-RUN systemctl mask dev-mqueue.mount dev-hugepages.mount \
-    systemd-remount-fs.service sys-kernel-config.mount \
-    sys-kernel-debug.mount sys-fs-fuse-connections.mount \
-    display-manager.service graphical.target systemd-logind.service
-
-ADD dbus.service /etc/systemd/system/dbus.service
-RUN systemctl enable dbus.service
-
-ENV LANG en_US.UTF-8
-
-RUN zypper -n addrepo -p 90 obs://devel:openQA devel:openQA
-RUN zypper -n addrepo -p 91 obs://devel:openQA:Leap:15.1 devel:openQA:Leap:15.1
-RUN zypper -n --gpg-auto-import-keys --no-gpg-checks refresh
-
-RUN zypper -n install --no-recommends perl-Mojo-SQLite
-RUN zypper -n install --no-recommends perl-CommonMark
-RUN zypper -n install --no-recommends perl-Mojolicious-Plugin-AssetPack
-RUN zypper -n install --no-recommends openQA-local-db apache2
-
-RUN zypper -n install apparmor-profiles apparmor-utils
-RUN zypper -n install curl hostname iputils vim command-not-found bsdtar zip
-
-RUN zypper -n install make
-
-VOLUME ['/sys/fs/cgroup']
-VOLUME ['/run']"
-
-[ ! -f "$dockerfile" ] || cat "$dockerfile"
-
-[ -z "$EXPOSE_PORT" ] || echo EXPOSE 80
-
-echo "CMD  ['/usr/lib/systemd/systemd']"
-
-) | docker build -t "$containername" -f- "$thisdir"
+PLAN 1
 
 map_port=""
 [ -z "$EXPOSE_PORT" ] || map_port="-p $EXPOSE_PORT:80"
-docker run --privileged --security-opt=seccomp:unconfined --security-opt=apparmor:unconfined $map_port -v"$thisdir/../../..":/opt/openqa-trigger-from-obs --rm --name "$containername" -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro -- "$containername" /usr/lib/systemd/systemd &
+# docker run --privileged $map_port -v"$thisdir/../../..":/opt/openqa-trigger-from-obs --env METHOD=$METHOD --rm --name "$containername" -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro -- registry.opensuse.org/devel/openqa/ci/containers/serviced &
+docker run --privileged $map_port -v"$thisdir/../../..":/opt/openqa-trigger-from-obs --env METHOD=$METHOD --rm --name "$containername" -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro -- 5467a81e9e28 &
 
 in_cleanup=0
 
@@ -103,7 +53,7 @@ function cleanup {
 }
 
 trap cleanup INT TERM EXIT
-counter=0
+counter=1
 
 # wait container start
 until [ $counter -gt 10 ]; do
@@ -114,22 +64,10 @@ done
 
 docker exec "$containername" pwd >& /dev/null || (echo Cannot start container; exit 1 ) >&2
 
-# starting services in container does enable apparmor profile
-# on host, so we must disable the profile again
-# so it doesn't interfere with the test
-# (feel free to remove this line if it is not needed)
-
-echo 'cd /opt/openqa-trigger-from-obs && make install_apparmor' | docker exec -i "$containername" bash -x
-# these are hacks to make apparmor tolerate container
-echo 'sed -i "s,/usr/share/openqa/script/openqa {,/usr/share/openqa/script/openqa flags=(attach_disconnected) {," /etc/apparmor.d/usr.share.openqa.script.openqa' | docker exec -i "$containername" bash -x
-echo 'echo " /var/lib/docker/** r," >> /etc/apparmor.d/local/usr.share.openqa.script.openqa' | docker exec -i "$containername" bash -x
-echo 'sed -i "s,/opt/openqa-trigger-from-obs/script/rsync.sh {,/opt/openqa-trigger-from-obs/script/rsync.sh flags=(attach_disconnected) {," /etc/apparmor.d/opt.openqa-trigger-from-obs.script.rsync.sh' | docker exec -i "$containername" bash -x
-echo 'sed -i "/\/usr\/bin\/rsync mrix,/a  \/var\/lib\/docker\/** r," /etc/apparmor.d/opt.openqa-trigger-from-obs.script.rsync.sh' | docker exec -i "$containername" bash -x
-echo 'echo " /var/lib/docker/** r," >> /etc/apparmor.d/local/opt.openqa-trigger-from-obs.script.rsync.sh' | docker exec -i "$containername" bash -x
-echo 'rcapparmor restart' | docker exec -i "$containername" bash -x
-
+echo 'bash /opt/init-trigger-from-obs.sh' | docker exec -i "$containername" bash -x
 
 set +e
+docker cp lib/common.sh "$containername":/lib
 docker exec -i "$containername" bash < "$testcase"
 ret=$?
 IS $ret == 0 # test execution
