dffd73704eeba4987bfde2f41cdb166b3ac9281f
Martin Loviska <mloviska@suse.com>
2022-05-25 17:28:01 +0200

    Remove EoL Leap WSL schedule

--- t/docker/rsync.sh
+++ t/docker/rsync.sh
@@ -3,4 +3,4 @@ set -e
 script/openSUSE:Leap:15.2:Staging:A.sh
 script/openSUSE:Leap:15.2:ARM:ToTest.sh
 script/openSUSE:Factory:ToTest.sh
-script/openSUSE:Leap:15.2:WSL.sh
+script/openSUSE:Leap:15.4:WSL.sh
