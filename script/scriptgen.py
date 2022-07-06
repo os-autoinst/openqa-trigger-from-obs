os-autoinst/openqa-trigger-from-obs

commit 9c317062d5dca605025e685f6278513412e020b5

Start scheduling ALP product

script/scriptgen.py
@@ -607,7 +607,7 @@ class ActionBatch:
                     cfg.read_files_repo,
                     f,
                     txt,
-                    self.ag.productpath + "/" + self.folder + "/*" + repodir.attrib["folder"] + "*",
+                    self.ag.productpath + "/" + self.folder + "/*" + repodir.attrib["folder"] + "*/",
                     "REPOORS",
                     "",
                     "files_repo.lst",
@@ -617,7 +617,7 @@ class ActionBatch:
                 )
 
         # let's sync media.1/media to be able verify build_id
-        if "ToTest" in self.ag.envdir or self.version_from_media:
+        if "ToTest" or "LEO" in self.ag.envdir or self.version_from_media:
             archs = self.archs
             if not archs:
                 archs = self.ag.archs
@@ -642,7 +642,7 @@ class ActionBatch:
                             os.path.basename(repodir.attrib["folder"]).lstrip("*") + repodir.get("archs", "")
                         ),
                     )
-            elif "Factory" in self.ag.envdir:
+            elif "Factory" or "LEO" in self.ag.envdir:
                 for repodir in self.repodirs:
                     if not repodir.attrib.get("gen", ""):
                         self.p(

t/obs/devel:LEO/base/files_iso.lst
@@ -1,0 +1,2 @@
+ALP.x86_64-0.1-SelfInstall-Build17.1.install.iso
+ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.1.install.iso

t/obs/devel:LEO/base/files_repo_ALP-ftp-POOL.lst
@@ -1,0 +1,4 @@
+ALP-0.1-x86_64-Build258.1-Media1.license
+ALP-0.1-x86_64-Build258.1-Media1
+ALP-0.1-x86_64-Build258.1-Media2
+ALP-0.1-x86_64-Build258.1-Media3

t/obs/devel:LEO/base/print_openqa.before
@@ -1,0 +1,28 @@
+/usr/bin/openqa-cli api -X post isos?async=1 \
+ ARCH=x86_64 \
+ ASSET_256=ALP.x86_64-0.1-SelfInstall-Build17.1.install.iso.sha256 \
+ BUILD=17.1 \
+ CHECKSUM_ISO=$(cut -b-64 /var/lib/openqa/factory/other/ALP.x86_64-0.1-SelfInstall-Build17.1.install.iso.sha256 | grep -E '[0-9a-f]{5,40}' | head -n1) \
+ DISTRI=alp \
+ FLAVOR=SelfInstall \
+ ISO=ALP.x86_64-0.1-SelfInstall-Build17.1.install.iso \
+ REPO_0=ALP-0.1-x86_64-Build17.1 \
+ REPO_1=ALP-0.1-x86_64-Build17.1 \
+ REPO_ALP=ALP-0.1-x86_64-Build17.1 \
+ VERSION=0.1 \
+ _OBSOLETE=1
+
+/usr/bin/openqa-cli api -X post isos?async=1 \
+ ARCH=x86_64 \
+ ASSET_256=ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.1.install.iso.sha256 \
+ BUILD=17.1 \
+ CHECKSUM_ISO=$(cut -b-64 /var/lib/openqa/factory/other/ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.1.install.iso.sha256 | grep -E '[0-9a-f]{5,40}' | head -n1) \
+ DISTRI=alp \
+ FLAVOR=SelfInstall_NonTransactional \
+ ISO=ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.1.install.iso \
+ REPO_0=ALP-0.1-x86_64-Build17.1 \
+ REPO_1=ALP-0.1-x86_64-Build17.1 \
+ REPO_ALP=ALP-0.1-x86_64-Build17.1 \
+ VERSION=0.1 \
+ _OBSOLETE=1
+

t/obs/devel:LEO/base/print_rsync_iso.before
@@ -1,0 +1,4 @@
+rsync --timeout=3600 -tlp4 --specials obspublish-other::openqa/devel:LEO/images/*/ALP:SelfInstall/*ALP.x86_64-0.1-SelfInstall-Build17.1.install.iso /var/lib/openqa/factory/iso/ALP.x86_64-0.1-SelfInstall-Build17.1.install.iso
+rsync --timeout=3600 -tlp4 --specials obspublish-other::openqa/devel:LEO/images/*/ALP:SelfInstall/*ALP.x86_64-0.1-SelfInstall-Build17.1.install.iso.sha256 /var/lib/openqa/factory/other/ALP.x86_64-0.1-SelfInstall-Build17.1.install.iso.sha256
+rsync --timeout=3600 -tlp4 --specials obspublish-other::openqa/devel:LEO/images/*/ALP:SelfInstall_NonTransactional/*ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.1.install.iso /var/lib/openqa/factory/iso/ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.1.install.iso
+rsync --timeout=3600 -tlp4 --specials obspublish-other::openqa/devel:LEO/images/*/ALP:SelfInstall_NonTransactional/*ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.1.install.iso.sha256 /var/lib/openqa/factory/other/ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.1.install.iso.sha256

t/obs/devel:LEO/base/print_rsync_repo.before
@@ -1,0 +1,4 @@
+rsync --timeout=3600 -rtlp4 --delete --specials obspublish-other::openqa/devel:LEO/images/*/*ALP-ftp-POOL*x86_64*/*Media1/* /var/lib/openqa/factory/repo/ALP-0.1-x86_64-CURRENT/
+rsync --timeout=3600 -rtlp4 --delete --specials --link-dest /var/lib/openqa/factory/repo/ALP-0.1-x86_64-CURRENT/ /var/lib/openqa/factory/repo/ALP-0.1-x86_64-CURRENT/ /var/lib/openqa/factory/repo/ALP-0.1-x86_64-Build17.1
+rsync --timeout=3600 -rtlp4 --delete --specials obspublish-other::openqa/devel:LEO/images/*/*ALP-ftp-POOL*x86_64*/*Media1/* /var/lib/openqa/factory/repo/ALP-0.1-x86_64-CURRENT/
+rsync --timeout=3600 -rtlp4 --delete --specials --link-dest /var/lib/openqa/factory/repo/ALP-0.1-x86_64-CURRENT/ /var/lib/openqa/factory/repo/ALP-0.1-x86_64-CURRENT/ /var/lib/openqa/factory/repo/ALP-0.1-x86_64-Build17.1

t/obs/devel:LEO/other/files_iso.lst
@@ -1,0 +1,2 @@
+ALP.x86_64-0.1-kvm-and-xen-Build17.1.qcow2
+ALP.x86_64-0.1-kvm-and-xen_NonTransactional-Build17.1.qcow2

t/obs/devel:LEO/other/files_repo_ALP-ftp-POOL.lst
@@ -1,0 +1,4 @@
+ALP-0.1-x86_64-Build258.1-Media1.license
+ALP-0.1-x86_64-Build258.1-Media1
+ALP-0.1-x86_64-Build258.1-Media2
+ALP-0.1-x86_64-Build258.1-Media3

commit 25ba5e358758e9ab5fabc8770ac279cebb6b2d73

Fix regression caused by #170
Instead of forcing the `/` we provide it in the xml as a suffix.

script/scriptgen.py
@@ -603,11 +603,14 @@ class ActionBatch:
                 txt = "PRODUCTREPOPATH"
                 if self.version_from_media:
                     txt = "PRODUCTREPOPATH/"
+                suffix = ""
+                if repodir.attrib.get("suffix"):
+                    suffix = repodir.attrib["suffix"]
                 self.p(
                     cfg.read_files_repo,
                     f,
                     txt,
-                    self.ag.productpath + "/" + self.folder + "/*" + repodir.attrib["folder"] + "*/",
+                    self.ag.productpath + "/" + self.folder + "/*" + repodir.attrib["folder"] + "*" + suffix,
                     "REPOORS",
                     "",
                     "files_repo.lst",

t/obs/devel:LEO/base/files_iso.lst
@@ -1,2 +1,2 @@
-ALP.x86_64-0.1-SelfInstall-Build17.10.install.iso
-ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.10.install.iso
+ALP.x86_64-0.1-SelfInstall-Build17.18.install.iso
+ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.18.install.iso

t/obs/devel:LEO/base/files_repo_ALP-ftp-POOL.lst
@@ -1,4 +1,4 @@
-ALP-0.1-x86_64-Build258.8-Media1.license
-ALP-0.1-x86_64-Build258.8-Media1
-ALP-0.1-x86_64-Build258.8-Media2
-ALP-0.1-x86_64-Build258.8-Media3
+ALP-0.1-x86_64-Build260.3-Media1.license
+ALP-0.1-x86_64-Build260.3-Media1
+ALP-0.1-x86_64-Build260.3-Media2
+ALP-0.1-x86_64-Build260.3-Media3

t/obs/devel:LEO/base/print_openqa.before
@@ -1,28 +1,28 @@
 /usr/bin/openqa-cli api -X post isos?async=1 \
  ARCH=x86_64 \
- ASSET_256=ALP.x86_64-0.1-SelfInstall-Build17.10.install.iso.sha256 \
- BUILD=17.10 \
- CHECKSUM_ISO=$(cut -b-64 /var/lib/openqa/factory/other/ALP.x86_64-0.1-SelfInstall-Build17.10.install.iso.sha256 | grep -E '[0-9a-f]{5,40}' | head -n1) \
+ ASSET_256=ALP.x86_64-0.1-SelfInstall-Build17.18.install.iso.sha256 \
+ BUILD=17.18 \
+ CHECKSUM_ISO=$(cut -b-64 /var/lib/openqa/factory/other/ALP.x86_64-0.1-SelfInstall-Build17.18.install.iso.sha256 | grep -E '[0-9a-f]{5,40}' | head -n1) \
  DISTRI=alp \
  FLAVOR=SelfInstall \
- ISO=ALP.x86_64-0.1-SelfInstall-Build17.10.install.iso \
- REPO_0=ALP-0.1-x86_64-Build17.10 \
- REPO_1=ALP-0.1-x86_64-Build17.10 \
- REPO_ALP=ALP-0.1-x86_64-Build17.10 \
+ ISO=ALP.x86_64-0.1-SelfInstall-Build17.18.install.iso \
+ REPO_0=ALP-0.1-x86_64-Build17.18 \
+ REPO_1=ALP-0.1-x86_64-Build17.18 \
+ REPO_ALP=ALP-0.1-x86_64-Build17.18 \
  VERSION=0.1 \
  _OBSOLETE=1
 
 /usr/bin/openqa-cli api -X post isos?async=1 \
  ARCH=x86_64 \
- ASSET_256=ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.10.install.iso.sha256 \
- BUILD=17.10 \
- CHECKSUM_ISO=$(cut -b-64 /var/lib/openqa/factory/other/ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.10.install.iso.sha256 | grep -E '[0-9a-f]{5,40}' | head -n1) \
+ ASSET_256=ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.18.install.iso.sha256 \
+ BUILD=17.18 \
+ CHECKSUM_ISO=$(cut -b-64 /var/lib/openqa/factory/other/ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.18.install.iso.sha256 | grep -E '[0-9a-f]{5,40}' | head -n1) \
  DISTRI=alp \
  FLAVOR=SelfInstall_NonTransactional \
- ISO=ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.10.install.iso \
- REPO_0=ALP-0.1-x86_64-Build17.10 \
- REPO_1=ALP-0.1-x86_64-Build17.10 \
- REPO_ALP=ALP-0.1-x86_64-Build17.10 \
+ ISO=ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.18.install.iso \
+ REPO_0=ALP-0.1-x86_64-Build17.18 \
+ REPO_1=ALP-0.1-x86_64-Build17.18 \
+ REPO_ALP=ALP-0.1-x86_64-Build17.18 \
  VERSION=0.1 \
  _OBSOLETE=1
 

t/obs/devel:LEO/base/print_rsync_iso.before
@@ -1,4 +1,4 @@
-rsync --timeout=3600 -tlp4 --specials obspublish-other::openqa/devel:LEO/images/*/ALP:SelfInstall/*ALP.x86_64-0.1-SelfInstall-Build17.10.install.iso /var/lib/openqa/factory/iso/ALP.x86_64-0.1-SelfInstall-Build17.10.install.iso
-rsync --timeout=3600 -tlp4 --specials obspublish-other::openqa/devel:LEO/images/*/ALP:SelfInstall/*ALP.x86_64-0.1-SelfInstall-Build17.10.install.iso.sha256 /var/lib/openqa/factory/other/ALP.x86_64-0.1-SelfInstall-Build17.10.install.iso.sha256
-rsync --timeout=3600 -tlp4 --specials obspublish-other::openqa/devel:LEO/images/*/ALP:SelfInstall_NonTransactional/*ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.10.install.iso /var/lib/openqa/factory/iso/ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.10.install.iso
-rsync --timeout=3600 -tlp4 --specials obspublish-other::openqa/devel:LEO/images/*/ALP:SelfInstall_NonTransactional/*ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.10.install.iso.sha256 /var/lib/openqa/factory/other/ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.10.install.iso.sha256
+rsync --timeout=3600 -tlp4 --specials obspublish-other::openqa/devel:LEO/images/*/ALP:SelfInstall/*ALP.x86_64-0.1-SelfInstall-Build17.18.install.iso /var/lib/openqa/factory/iso/ALP.x86_64-0.1-SelfInstall-Build17.18.install.iso
+rsync --timeout=3600 -tlp4 --specials obspublish-other::openqa/devel:LEO/images/*/ALP:SelfInstall/*ALP.x86_64-0.1-SelfInstall-Build17.18.install.iso.sha256 /var/lib/openqa/factory/other/ALP.x86_64-0.1-SelfInstall-Build17.18.install.iso.sha256
+rsync --timeout=3600 -tlp4 --specials obspublish-other::openqa/devel:LEO/images/*/ALP:SelfInstall_NonTransactional/*ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.18.install.iso /var/lib/openqa/factory/iso/ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.18.install.iso
+rsync --timeout=3600 -tlp4 --specials obspublish-other::openqa/devel:LEO/images/*/ALP:SelfInstall_NonTransactional/*ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.18.install.iso.sha256 /var/lib/openqa/factory/other/ALP.x86_64-0.1-SelfInstall_NonTransactional-Build17.18.install.iso.sha256

t/obs/devel:LEO/base/print_rsync_repo.before
@@ -1,4 +1,4 @@
 rsync --timeout=3600 -rtlp4 --delete --specials obspublish-other::openqa/devel:LEO/images/*/*ALP-ftp-POOL*x86_64*/*Media1/* /var/lib/openqa/factory/repo/ALP-0.1-x86_64-CURRENT/
-rsync --timeout=3600 -rtlp4 --delete --specials --link-dest /var/lib/openqa/factory/repo/ALP-0.1-x86_64-CURRENT/ /var/lib/openqa/factory/repo/ALP-0.1-x86_64-CURRENT/ /var/lib/openqa/factory/repo/ALP-0.1-x86_64-Build17.10
+rsync --timeout=3600 -rtlp4 --delete --specials --link-dest /var/lib/openqa/factory/repo/ALP-0.1-x86_64-CURRENT/ /var/lib/openqa/factory/repo/ALP-0.1-x86_64-CURRENT/ /var/lib/openqa/factory/repo/ALP-0.1-x86_64-Build17.18
 rsync --timeout=3600 -rtlp4 --delete --specials obspublish-other::openqa/devel:LEO/images/*/*ALP-ftp-POOL*x86_64*/*Media1/* /var/lib/openqa/factory/repo/ALP-0.1-x86_64-CURRENT/
-rsync --timeout=3600 -rtlp4 --delete --specials --link-dest /var/lib/openqa/factory/repo/ALP-0.1-x86_64-CURRENT/ /var/lib/openqa/factory/repo/ALP-0.1-x86_64-CURRENT/ /var/lib/openqa/factory/repo/ALP-0.1-x86_64-Build17.10
+rsync --timeout=3600 -rtlp4 --delete --specials --link-dest /var/lib/openqa/factory/repo/ALP-0.1-x86_64-CURRENT/ /var/lib/openqa/factory/repo/ALP-0.1-x86_64-CURRENT/ /var/lib/openqa/factory/repo/ALP-0.1-x86_64-Build17.18

t/obs/devel:LEO/other/files_iso.lst
@@ -1,2 +1,2 @@
-ALP.x86_64-0.1-kvm-and-xen-Build17.10.qcow2
-ALP.x86_64-0.1-kvm-and-xen_NonTransactional-Build17.10.qcow2
+ALP.x86_64-0.1-kvm-and-xen-Build17.18.qcow2
+ALP.x86_64-0.1-kvm-and-xen_NonTransactional-Build17.18.qcow2

t/obs/devel:LEO/other/files_repo_ALP-ftp-POOL.lst
@@ -1,4 +1,4 @@
-ALP-0.1-x86_64-Build258.8-Media1.license
-ALP-0.1-x86_64-Build258.8-Media1
-ALP-0.1-x86_64-Build258.8-Media2
-ALP-0.1-x86_64-Build258.8-Media3
+ALP-0.1-x86_64-Build260.3-Media1.license
+ALP-0.1-x86_64-Build260.3-Media1
+ALP-0.1-x86_64-Build260.3-Media2
+ALP-0.1-x86_64-Build260.3-Media3

t/obs/devel:LEO/other/print_openqa.before
@@ -1,28 +1,28 @@
 /usr/bin/openqa-cli api -X post isos?async=1 \
  ARCH=x86_64 \
- ASSET_256=ALP.x86_64-0.1-kvm-and-xen-Build17.10.qcow2.sha256 \
- BUILD=17.10 \
- CHECKSUM_HDD_1=$(cut -b-64 /var/lib/openqa/factory/other/ALP.x86_64-0.1-kvm-and-xen-Build17.10.qcow2.sha256 | grep -E '[0-9a-f]{5,40}' | head -n1) \
+ ASSET_256=ALP.x86_64-0.1-kvm-and-xen-Build17.18.qcow2.sha256 \
+ BUILD=17.18 \
+ CHECKSUM_HDD_1=$(cut -b-64 /var/lib/openqa/factory/other/ALP.x86_64-0.1-kvm-and-xen-Build17.18.qcow2.sha256 | grep -E '[0-9a-f]{5,40}' | head -n1) \
  DISTRI=alp \
  FLAVOR=kvm-and-xen \
- HDD_1=ALP.x86_64-0.1-kvm-and-xen-Build17.10.qcow2 \
- REPO_0=ALP-0.1-x86_64-Build17.10 \
- REPO_1=ALP-0.1-x86_64-Build17.10 \
- REPO_ALP=ALP-0.1-x86_64-Build17.10 \
+ HDD_1=ALP.x86_64-0.1-kvm-and-xen-Build17.18.qcow2 \
+ REPO_0=ALP-0.1-x86_64-Build17.18 \
+ REPO_1=ALP-0.1-x86_64-Build17.18 \
+ REPO_ALP=ALP-0.1-x86_64-Build17.18 \
  VERSION=0.1 \
  _OBSOLETE=1
 
 /usr/bin/openqa-cli api -X post isos?async=1 \
  ARCH=x86_64 \
- ASSET_256=ALP.x86_64-0.1-kvm-and-xen_NonTransactional-Build17.10.qcow2.sha256 \
- BUILD=17.10 \
- CHECKSUM_HDD_1=$(cut -b-64 /var/lib/openqa/factory/other/ALP.x86_64-0.1-kvm-and-xen_NonTransactional-Build17.10.qcow2.sha256 | grep -E '[0-9a-f]{5,40}' | head -n1) \
+ ASSET_256=ALP.x86_64-0.1-kvm-and-xen_NonTransactional-Build17.18.qcow2.sha256 \
+ BUILD=17.18 \
+ CHECKSUM_HDD_1=$(cut -b-64 /var/lib/openqa/factory/other/ALP.x86_64-0.1-kvm-and-xen_NonTransactional-Build17.18.qcow2.sha256 | grep -E '[0-9a-f]{5,40}' | head -n1) \
  DISTRI=alp \
  FLAVOR=kvm-and-xen_NonTransactional \
- HDD_1=ALP.x86_64-0.1-kvm-and-xen_NonTransactional-Build17.10.qcow2 \
- REPO_0=ALP-0.1-x86_64-Build17.10 \
- REPO_1=ALP-0.1-x86_64-Build17.10 \
- REPO_ALP=ALP-0.1-x86_64-Build17.10 \
+ HDD_1=ALP.x86_64-0.1-kvm-and-xen_NonTransactional-Build17.18.qcow2 \
+ REPO_0=ALP-0.1-x86_64-Build17.18 \
+ REPO_1=ALP-0.1-x86_64-Build17.18 \
+ REPO_ALP=ALP-0.1-x86_64-Build17.18 \
  VERSION=0.1 \
  _OBSOLETE=1
 

t/obs/devel:LEO/other/print_rsync_iso.before
@@ -1,4 +1,4 @@
-rsync --timeout=3600 -tlp4 --specials obspublish-other::openqa/devel:LEO/images/*/ALP:kvm-and-xen/*ALP.x86_64-0.1-kvm-and-xen-Build17.10.qcow2 /var/lib/openqa/factory/hdd/ALP.x86_64-0.1-kvm-and-xen-Build17.10.qcow2
-rsync --timeout=3600 -tlp4 --specials obspublish-other::openqa/devel:LEO/images/*/ALP:kvm-and-xen/*ALP.x86_64-0.1-kvm-and-xen-Build17.10.qcow2.sha256 /var/lib/openqa/factory/other/ALP.x86_64-0.1-kvm-and-xen-Build17.10.qcow2.sha256
-rsync --timeout=3600 -tlp4 --specials obspublish-other::openqa/devel:LEO/images/*/ALP:kvm-and-xen_NonTransactional/*ALP.x86_64-0.1-kvm-and-xen_NonTransactional-Build17.10.qcow2 /var/lib/openqa/factory/hdd/ALP.x86_64-0.1-kvm-and-xen_NonTransactional-Build17.10.qcow2
-rsync --timeout=3600 -tlp4 --specials obspublish-other::openqa/devel:LEO/images/*/ALP:kvm-and-xen_NonTransactional/*ALP.x86_64-0.1-kvm-and-xen_NonTransactional-Build17.10.qcow2.sha256 /var/lib/openqa/factory/other/ALP.x86_64-0.1-kvm-and-xen_NonTransactional-Build17.10.qcow2.sha256
+rsync --timeout=3600 -tlp4 --specials obspublish-other::openqa/devel:LEO/images/*/ALP:kvm-and-xen/*ALP.x86_64-0.1-kvm-and-xen-Build17.18.qcow2 /var/lib/openqa/factory/hdd/ALP.x86_64-0.1-kvm-and-xen-Build17.18.qcow2
+rsync --timeout=3600 -tlp4 --specials obspublish-other::openqa/devel:LEO/images/*/ALP:kvm-and-xen/*ALP.x86_64-0.1-kvm-and-xen-Build17.18.qcow2.sha256 /var/lib/openqa/factory/other/ALP.x86_64-0.1-kvm-and-xen-Build17.18.qcow2.sha256
+rsync --timeout=3600 -tlp4 --specials obspublish-other::openqa/devel:LEO/images/*/ALP:kvm-and-xen_NonTransactional/*ALP.x86_64-0.1-kvm-and-xen_NonTransactional-Build17.18.qcow2 /var/lib/openqa/factory/hdd/ALP.x86_64-0.1-kvm-and-xen_NonTransactional-Build17.18.qcow2
+rsync --timeout=3600 -tlp4 --specials obspublish-other::openqa/devel:LEO/images/*/ALP:kvm-and-xen_NonTransactional/*ALP.x86_64-0.1-kvm-and-xen_NonTransactional-Build17.18.qcow2.sha256 /var/lib/openqa/factory/other/ALP.x86_64-0.1-kvm-and-xen_NonTransactional-Build17.18.qcow2.sha256