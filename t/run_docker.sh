16749403541b02c02075e1cf269459fd3ee8ce71
Cris Dywan <cris@twotoasts.de>
2022-12-06 18:26:19 +0100

    Retry failing docker tests
    See: https://progress.opensuse.org/issues/120468

--- t/run_docker.sh
+++ t/run_docker.sh
@@ -5,7 +5,9 @@ dockerfail=0
 cd docker
 for t in *.sh; do
     [ -x "$t" ] || continue
-    ./$t
+    for i in $(seq 1 3); do
+        ./$t && s=0 && break || s=$? && sleep 10;
+    done
     if [ $? -eq 0 ] ; then
         echo "P:$t"
     else
