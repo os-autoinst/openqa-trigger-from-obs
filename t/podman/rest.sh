808d538ed33ac89e89ffe2ba1dad8e9b80749ed6
Jozef Pupava <jpupava@suse.com>
2025-02-05 14:53:18 +0100

    Cleanup old Leap EOL projects
    https://progress.opensuse.org/issues/175704

--- t/podman/rest.sh
+++
@@ -1,4 +0,0 @@
-export METHOD=rest
-set -e
-script/openSUSE:Leap:15.2:Staging:A.sh
-script/openSUSE:Leap:15.4:WSL.sh
--- t/podman/rsync.sh
+++ t/podman/rsync.sh
@@ -1,6 +1,3 @@
 export METHOD=rsync
 set -e
-script/openSUSE:Leap:15.2:Staging:A.sh
-script/openSUSE:Leap:15.2:ARM:ToTest.sh
 script/openSUSE:Factory:ToTest.sh
-script/openSUSE:Leap:15.4:WSL.sh
