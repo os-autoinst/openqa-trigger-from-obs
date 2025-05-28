b87ee4c3fdcb5efa1d10bae6e7e01933764269a1
Santiago Zarate <santiago.zarate@suse.com>
2025-05-28 16:19:01 +0200

    Fix style issues

--- script/scriptgen.py
+++ script/scriptgen.py
@@ -1042,7 +1042,7 @@ done < <(sort __envsub/files_asset.lst)""",
                 )
                 imultiarch = imultiarch + 1
             if imultiarch > 0:
-                self.p(cfg.openqa_call_repo_unconditional(), f, "REPO0_ISO", '{}-$buildex'.format(dest))
+                self.p(cfg.openqa_call_repo_unconditional(), f, "REPO0_ISO", "{}-$buildex".format(dest))
 
         if len(self.iso1) > 0:
             self.p(' iso1=""', f)
